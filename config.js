c90c875aee02e16935985adfc6bb9c7418c33923
WILKER OFC <developer.wilker.ofc@gmail.com>
2026-02-11 21:38:15 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -37,6 +37,19 @@ global.ch = {
 global.multiplier = 69
 global.maxwarn = 2
 
+global.APIs = {
+  adonix: { url: "https://api-adonix.ultraplus.click", key: "KEYGOHANBOT" },
+  vreden: { url: "https://api.vreden.web.id", key: null },
+  nekolabs: { url: "https://api.nekolabs.web.id", key: null },
+  siputzx: { url: "https://api.siputzx.my.id", key: null },
+  delirius: { url: "https://api.delirius.store", key: null },
+  ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+  stellar: { url: "https://api.stellarwa.xyz", key: "YukiWaBot", key2: '1bcd4698ce6c75217275c9607f01fd99' },
+  apifaa: { url: "https://api-faa.my.id", key: null },
+  xyro: { url: "https://api.xyro.site", key: null },
+  yupra: { url: "https://api.yupra.my.id", key: null }
+}
+
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
